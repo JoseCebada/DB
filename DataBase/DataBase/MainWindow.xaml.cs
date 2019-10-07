cfdf87ad8ff5cbc9afea7ec2e6742b5fc0003558
Alumno <Alumno@PC27>
2019-10-07 10:19:58 -0500

    Clase Lunes

--- DataBase/DataBase/MainWindow.xaml.cs
+++ DataBase/DataBase/MainWindow.xaml.cs
@@ -29,12 +29,72 @@ namespace DataBase
             InitializeComponent();
             con = new OleDbConnection();
             con.ConnectionString = "Provider=Microsoft.Jet.Oledb.4.0; Data Source=" + AppDomain.CurrentDomain.BaseDirectory + "\\AlumnosDB";
+            MostrarDatos();
+        }
 
+        //Mostramos los registros de la tabla
+        private void MostrarDatos()
+        {
+            OleDbCommand cmd = new OleDbCommand();
+            if (con.State != ConnectionState.Open)
+                con.Open();
+            cmd.Connection = con;
+            cmd.CommandText = "select * from Progra";
+            OleDbDataAdapter da = new OleDbDataAdapter();
+            dt = new DataTable();
+            da.Fill(dt);
+            gvDatos.ItemsSource = dt.AsDataView();
+            if (dt.Rows.Count > 0)
+            {
+                lbContenido.Visibility = System.Windows.Visibility.Hidden;
+                gvDatos.Visibility = System.Windows.Visibility.Visible;
+            }
+            else
+            {
+                lbContenido.Visibility = System.Windows.Visibility.Visible;
+                gvDatos.Visibility = System.Windows.Visibility.Hidden;
+            }
+        }
+
+        private void LimpiarTodo()
+        {
+            txtId.Text = "";
+            txtNombre.Text = "";
+            cbGenero.SelectedIndex = 0;
+            txtTelefono.Text = "";
+            txtDireccion.Text = "";
+            btnNuevo.Content = "Nuevo";
+            txtId.IsEnabled = true;
         }
 
         private void BtnNuevo_Click(object sender, RoutedEventArgs e)
         {
-
+            OleDbCommand cmd = new OleDbCommand();
+            if (con.State != ConnectionState.Open)
+                con.Open();
+            cmd.Connection = con;
+            if (txtId.Text != "")
+            {
+                if(cbGenero.Text != "Selecciona Genero")
+                {
+                    cmd.CommandText = "insert into Progra(Id, Nombre, Genero, Telefono, Direccion)" + "Values(" + txtId + ", '" + txtNombre + ", '" + cbGenero + ", '" + txtTelefono + ", '" + txtDireccion + "')";
+                    cmd.ExecuteNonQuery();
+                    MessageBox.Show("Alumno agregado correctamente...");
+                    LimpiarTodo();
+                }
+                else
+                {
+                    MessageBox.Show("Favor de seleccionar el genero...");
+                }
+            }
+            else
+            {
+                cmd.CommandText = "update Progra set Nombre='" + txtNombre.Text + "', Genero='" + cbGenero.Text + "', Telefono = '" + txtTelefono.Text + "', Direccion= '" + txtDireccion.Text + "')";
+                cmd.ExecuteNonQuery();
+                MostrarDatos();
+                MessageBox.Show("Datos del alumno Actualizados...");
+                LimpiarTodo();
+            }
         }
 
         private void BtnEditar_Click(object sender, RoutedEventArgs e)
